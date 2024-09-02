dc788cf63778445ed81ac21c45d570682f02811f
SMK <dev.miggyu@gmail.com>
2024-09-02 20:18:40 +0900

    feat: change package name

--- build-logic/src/main/java/com/mingg/buildlogic/etc/Versions.kt
+++ build-logic/src/main/java/com/mingg/buildlogic/etc/Versions.kt
@@ -1,8 +1,8 @@
 package com.mingg.buildlogic.etc
 
 object Versions {
-    const val VERSION_CODE = 1
-    const val VERSION_NAME = "1.0.0"
+    const val VERSION_CODE = 2
+    const val VERSION_NAME = "1.0.1"
     const val COMPILE_SDK = 34
     const val MIN_SDK = 26
     const val TARGET_SDK = 34
