9c16a7f02bba00efcfc56dfa5c1ad7ca825fe697
SMK <dev.miggyu@gmail.com>
2024-09-30 17:06:33 +0900

    refactor: mvvm to mvi (ticker detail)

--- app/src/main/java/com/mingg/coincheck/ui/tickerdetail/TickerDetailActivity.kt
+++ app/src/main/java/com/mingg/coincheck/ui/tickerdetail/TickerDetailActivity.kt
@@ -7,40 +7,47 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.webkit.WebViewClient
+import android.widget.TextView
 import androidx.activity.viewModels
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.lifecycleScope
 import com.mingg.coincheck.R
 import com.mingg.coincheck.databinding.ActivityTickerDetailBinding
-import com.mingg.coincheck.extension.collectWithLifecycle
 import com.mingg.coincheck.model.myasset.MyTickerInfo
 import com.mingg.coincheck.ui.base.BaseActivity
 import com.mingg.coincheck.ui.myasset.dialog.AddMyAssetDialogFragment
 import com.mingg.coincheck.utils.AppThemeManager
 import com.mingg.coincheck.utils.TradingViewUtil
+import com.mingg.domain.model.ticker.Ticker
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
+import java.util.Locale
 
 @AndroidEntryPoint
 class TickerDetailActivity :
-    BaseActivity<ActivityTickerDetailBinding>(R.layout.activity_ticker_detail) {
-    private val _tickerDetailViewModel: TickerDetailViewModel by viewModels()
+    BaseActivity<ActivityTickerDetailBinding>(ActivityTickerDetailBinding::inflate) {
 
-    private var _myTickerInfo: MyTickerInfo? = null
+    private val tickerDetailViewModel: TickerDetailViewModel by viewModels()
+
+    private var myTickerInfo: MyTickerInfo? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        _myTickerInfo = intent.getParcelableExtra(KEY_MY_TICKER)
+        myTickerInfo = intent.getParcelableExtra(KEY_MY_TICKER)
 
         setupActionBar()
         setupWebView()
-        setupObserver()
-        observeTicker()
+        setupObservers()
+
+        myTickerInfo?.let {
+            tickerDetailViewModel.setEvent(TickerDetailIntent.LoadTicker(it.symbol, it.currency))
+        }
     }
 
     @SuppressLint("SetJavaScriptEnabled")
     private fun setupWebView() {
-        _myTickerInfo?.let {
+        myTickerInfo?.let {
             val symbol = it.symbol + it.currency.name
             binding.webviewChart.webViewClient = WebViewClient()
             binding.webviewChart.settings.javaScriptEnabled = true
@@ -61,20 +68,36 @@ class TickerDetailActivity :
         }
     }
 
-    private fun setupObserver() {
+    private fun setupObservers() {
         lifecycleScope.launch {
-            _tickerDetailViewModel.ticker.collectWithLifecycle(lifecycle) {
-                binding.ticker = it
+            tickerDetailViewModel.uiState.collect { state ->
+                state.ticker?.let { ticker ->
+                    binding.tvSymbol.text = getTickerSymbolText(ticker)
+                    binding.tvPrice.text = ticker.decimalCurrentPrice
+                    binding.tvPrevChange.text = "${ticker.changePricePrevDay} (${ticker.rate}%)"
+                    setTickerPriceColor(binding.tvPrice, ticker.rate)
+                    setTickerPriceColor(binding.tvPrevChange, ticker.rate)
+                }
             }
         }
     }
 
-    private fun observeTicker() {
-        _myTickerInfo?.let {
-            _tickerDetailViewModel.observeTicker(
-                it.symbol,
-                it.currency
-            )
+    private fun getTickerSymbolText(ticker: Ticker): String {
+        return if (Locale.getDefault().language == "ko") {
+            "${ticker.koreanSymbol} (${ticker.symbol})"
+        } else {
+            "${ticker.englishSymbol} (${ticker.symbol})"
+        }
+    }
+
+    private fun setTickerPriceColor(view: TextView, rate: String?) {
+        rate?.let {
+            val color: Int = when {
+                it.toFloat() > 0 -> ContextCompat.getColor(this, R.color.color_price_up)
+                it.toFloat() < 0 -> ContextCompat.getColor(this, R.color.color_price_down)
+                else -> ContextCompat.getColor(this, R.color.color_price_same)
+            }
+            view.setTextColor(color)
         }
     }
 
@@ -91,7 +114,7 @@ class TickerDetailActivity :
             }
 
             R.id.menu_add_my_asset -> {
-                _myTickerInfo?.let {
+                myTickerInfo?.let {
                     AddMyAssetDialogFragment.newInstance(it).show(supportFragmentManager, null)
                 }
             }
--- app/src/main/java/com/mingg/coincheck/ui/tickerdetail/TickerDetailViewModel.kt
+++ app/src/main/java/com/mingg/coincheck/ui/tickerdetail/TickerDetailViewModel.kt
@@ -1,28 +1,32 @@
 package com.mingg.coincheck.ui.tickerdetail
 
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.mingg.coincheck.ui.base.BaseViewModel
 import com.mingg.domain.model.ticker.Currency
-import com.mingg.domain.model.ticker.Ticker
 import com.mingg.domain.usecase.ticker.TickerSingleDataUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class TickerDetailViewModel @Inject constructor(
     private val tickerSingleDataUseCase: TickerSingleDataUseCase
-) : ViewModel() {
-    private val _ticker: MutableStateFlow<Ticker?> =
-        MutableStateFlow(null)
-    val ticker = _ticker.asStateFlow()
+) : BaseViewModel<TickerDetailState, TickerDetailIntent, TickerDetailEffect>() {
 
-    fun observeTicker(symbol: String, currency: Currency) {
+    override fun createInitialState(): TickerDetailState {
+        return TickerDetailState()
+    }
+
+    override fun handleEvent(event: TickerDetailIntent) {
+        when (event) {
+            is TickerDetailIntent.LoadTicker -> loadTicker(event.symbol, event.currency)
+        }
+    }
+
+    private fun loadTicker(symbol: String, currency: Currency) {
         viewModelScope.launch {
-            tickerSingleDataUseCase.execute(symbol, currency).collect {
-                _ticker.value = it
+            tickerSingleDataUseCase.execute(symbol, currency).collect { ticker ->
+                setState { copy(ticker = ticker) }
             }
         }
     }
