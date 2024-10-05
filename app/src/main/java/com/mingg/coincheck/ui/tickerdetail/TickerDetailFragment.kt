59906ea952ebc564fb4a5de2bc5d585570d58020
SMK <dev.miggyu@gmail.com>
2024-10-05 23:40:53 +0900

    feat: clean up tickerdetail

--- app/src/main/java/com/mingg/coincheck/ui/tickerdetail/TickerDetailFragment.kt
+++ app/src/main/java/com/mingg/coincheck/ui/tickerdetail/TickerDetailFragment.kt
@@ -10,6 +10,8 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import com.mingg.coincheck.R
 import com.mingg.coincheck.databinding.FragmentTickerDetailBinding
+import com.mingg.coincheck.extension.collectWithLifecycle
+import com.mingg.coincheck.extension.getParcelableCompat
 import com.mingg.coincheck.model.myasset.MyTickerInfo
 import com.mingg.coincheck.ui.base.BaseFragment
 import com.mingg.coincheck.ui.myasset.dialog.AddMyAssetDialogFragment
@@ -30,7 +32,7 @@ class TickerDetailFragment : BaseFragment<FragmentTickerDetailBinding>(FragmentT
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        myTickerInfo = arguments?.getParcelable(KEY_MY_TICKER)
+        myTickerInfo = arguments?.getParcelableCompat(KEY_MY_TICKER)
 
         setupListener()
         setupWebView()
@@ -58,28 +60,37 @@ class TickerDetailFragment : BaseFragment<FragmentTickerDetailBinding>(FragmentT
     private fun setupWebView() {
         myTickerInfo?.let {
             val symbol = it.symbol + it.currency.name
-            binding.webviewChart.webViewClient = WebViewClient()
-            binding.webviewChart.settings.javaScriptEnabled = true
-            binding.webviewChart.loadData(
-                TradingViewUtil.getScript(
-                    symbol = symbol,
-                    isDarkMode = AppThemeManager.isDarkMode(resources)
-                ), "text/html", "base64"
-            )
+            binding.webviewChart.apply {
+                webViewClient = WebViewClient()
+                settings.javaScriptEnabled = true
+                loadData(
+                    TradingViewUtil.getScript(
+                        symbol = symbol,
+                        isDarkMode = AppThemeManager.isDarkMode(resources)
+                    ), "text/html", "base64"
+                )
+            }
         }
     }
 
     private fun setupObservers() {
         lifecycleScope.launch {
-            tickerDetailViewModel.uiState.collect { state ->
+            tickerDetailViewModel.uiState.collectWithLifecycle(lifecycle) { state ->
                 state.ticker?.let { ticker ->
-                    binding.tvSymbol.text = getTickerSymbolText(ticker)
-                    binding.tvPrice.text = ticker.decimalCurrentPrice
-                    binding.tvPrevChange.text = "${ticker.changePricePrevDay} (${ticker.rate}%)"
-                    setTickerPriceColor(binding.tvPrice, ticker.rate)
-                    setTickerPriceColor(binding.tvPrevChange, ticker.rate)
+                    updateUI(ticker)
                 }
             }
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun updateUI(ticker: Ticker) {
+        binding.apply {
+            tvSymbol.text = getTickerSymbolText(ticker)
+            tvPrice.text = ticker.decimalCurrentPrice
+            tvPrevChange.text = "${ticker.changePricePrevDay} (${ticker.rate}%)"
+            tvPrice.setTickerPriceColor(ticker.rate)
+            tvPrevChange.setTickerPriceColor(ticker.rate)
         }
     }
 
@@ -91,15 +102,14 @@ class TickerDetailFragment : BaseFragment<FragmentTickerDetailBinding>(FragmentT
         }
     }
 
-    private fun setTickerPriceColor(view: TextView, rate: String?) {
-        rate?.let {
-            val color: Int = when {
-                it.toFloat() > 0 -> ContextCompat.getColor(requireContext(), R.color.color_price_up)
-                it.toFloat() < 0 -> ContextCompat.getColor(requireContext(), R.color.color_price_down)
-                else -> ContextCompat.getColor(requireContext(), R.color.color_price_same)
-            }
-            view.setTextColor(color)
+    private fun TextView.setTickerPriceColor(rate: String?) {
+        rate ?: return
+        val color: Int = when {
+            rate.toFloat() > 0 -> ContextCompat.getColor(requireContext(), R.color.color_price_up)
+            rate.toFloat() < 0 -> ContextCompat.getColor(requireContext(), R.color.color_price_down)
+            else -> ContextCompat.getColor(requireContext(), R.color.color_price_same)
         }
+        setTextColor(color)
     }
 
     companion object {
